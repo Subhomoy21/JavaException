118e42ab4039c793d595ac755c06772330c09c53
Subhomoy Das <subhomoydas54@gmail.com>
2024-06-06 09:34:50 +0530

    CustomExceptions

--- src/Exceptions/CustomExceptions.java
+++ src/Exceptions/CustomExceptions.java
@@ -1,7 +1,25 @@
 package Exceptions;
 
+import java.util.Scanner;
+
+class MyException extends Exception{
+        // constructor
+    public MyException(String message){
+        super(message);
+    }
+}
 public class CustomExceptions {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the age");
+        try{
+            int age = sc.nextInt();
+            if(age>80){
+                    throw new MyException("My error is this:");
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
